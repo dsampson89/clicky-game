61609e8f19c7f9eacc06b4c6e0de7816c24827eb
Dexter Sampson <dexter1.sampson@gmail.com>
2020-06-26 20:39:38 -0400

    Updates

---
+++ precache-manifest.92de97b2a1955c810a036c20bb31ebd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff248e8694308e0d1f61e64ec7d361ae",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "544a3e596d8425801052",
+    "url": "/clicky-game/static/css/main.6bedc1ca.chunk.css"
+  },
+  {
+    "revision": "fa16647add3b33dfa690",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544a3e596d8425801052",
+    "url": "/clicky-game/static/js/main.6acc7a4a.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
